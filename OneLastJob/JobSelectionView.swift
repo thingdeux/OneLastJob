0d8994f5598cee6a9bd73068e710f20864aaa8cf
Joshua Johnson <me@josh.land>
2021-06-07 02:38:58 -0700

    Added one flag for global state to help when demo'ing.

--- OneLastJob/JobSelectionView.swift
+++ OneLastJob/JobSelectionView.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 import MapKit
 
+var startedFinalDraw = false
+
 struct JobSelectionView: View {
+
     var body: some View {
         ScrollView {
             VStack(spacing: 20) {
-                RoulletteView(title: "Location", dataset: locationsDataSet, timeToResolveInSeconds: 10)
-                    .padding(.top, 90)
+                RoulletteView(title: "Location", dataset: locationsDataSet, timeToResolveInSeconds: startedFinalDraw ? 25 : 10)
+                    .padding(.top, 40)
 
-                RoulletteView(title: "Venue", dataset: venuesDataSet, timeToResolveInSeconds: 20)
-                RoulletteView(title: "Thing", dataset: thingDataSet, timeToResolveInSeconds: 35)
-                RoulletteView(title: "Time Limit", dataset: timeLimitDataset, timeToResolveInSeconds: 10)
-                RoulletteView(title: "Time of Day", dataset: timeOfDayDataset, timeToResolveInSeconds: 10)
+                RoulletteView(title: "Venue", dataset: venuesDataSet, timeToResolveInSeconds: startedFinalDraw ? 28 : 9)
+                RoulletteView(title: "Thing", dataset: thingDataSet, timeToResolveInSeconds: startedFinalDraw ? 40 : 4)
+                RoulletteView(title: "Time Limit", dataset: timeLimitDataset, timeToResolveInSeconds: startedFinalDraw ? 12 : 7)
+                RoulletteView(title: "Time of Day", dataset: timeOfDayDataset, timeToResolveInSeconds: startedFinalDraw ? 12 : 8)
 
                 Image("SonOfMan")
                     .resizable()
@@ -27,11 +30,11 @@ struct JobSelectionView: View {
                     .clipShape(RoundedRectangle(cornerRadius: 30))
                     .padding(.bottom, 40)
                     .onTapGesture {
-                        NotificationCenter.default.post(name: startRoulleteShuffle, object: nil)
+                        startedFinalDraw = true
+                        NotificationCenter.default.post(name: startRoulleteShuffle, object: self)
                     }
             }
         }
-
         .colorScheme(.dark)
         .frame(width: mainViewWidth, height: 1300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }

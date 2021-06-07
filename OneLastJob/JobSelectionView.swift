d5822a199ebf65b95dbeb60c8cf5352bd1a2f1dd
Joshua Johnson <me@josh.land>
2021-06-07 02:45:37 -0700

    Updated flag need state (core tenant of SwiftUI ... doh)!

--- OneLastJob/JobSelectionView.swift
+++ OneLastJob/JobSelectionView.swift
@@ -8,18 +8,17 @@
 import SwiftUI
 import MapKit
 
-private var startedFinalDraw = false
-
 struct JobSelectionView: View {
+    @State private var startedFinalDraw = false
 
     var body: some View {
         ScrollView {
             VStack(spacing: 20) {
-                RoulletteView(title: "Location", dataset: locationsDataSet, timeToResolveInSeconds: startedFinalDraw ? 25 : 10)
+                RoulletteView(title: "Location", dataset: locationsDataSet, timeToResolveInSeconds: startedFinalDraw ? 30 : 10)
                     .padding(.top, 40)
 
-                RoulletteView(title: "Venue", dataset: venuesDataSet, timeToResolveInSeconds: startedFinalDraw ? 28 : 9)
-                RoulletteView(title: "Thing", dataset: thingDataSet, timeToResolveInSeconds: startedFinalDraw ? 40 : 4)
+                RoulletteView(title: "Venue", dataset: venuesDataSet, timeToResolveInSeconds: startedFinalDraw ? 32 : 9)
+                RoulletteView(title: "Thing", dataset: thingDataSet, timeToResolveInSeconds: startedFinalDraw ? 36 : 4)
                 RoulletteView(title: "Time Limit", dataset: timeLimitDataset, timeToResolveInSeconds: startedFinalDraw ? 12 : 7)
                 RoulletteView(title: "Time of Day", dataset: timeOfDayDataset, timeToResolveInSeconds: startedFinalDraw ? 12 : 8)
 
@@ -31,7 +30,10 @@ struct JobSelectionView: View {
                     .padding(.bottom, 40)
                     .onTapGesture {
                         startedFinalDraw = true
-                        NotificationCenter.default.post(name: startRoulleteShuffle, object: self)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            // Allow for views to be recreated and resubscribe to listen.
+                            NotificationCenter.default.post(name: startRoulleteShuffle, object: self)
+                        }
                     }
             }
         }

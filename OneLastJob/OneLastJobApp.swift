2cc04e21bfd18e56f3cf8fde21f2ca26650a5e28
Joshua Johnson <me@josh.land>
2021-06-06 20:51:24 -0700

    Initial Commit

---
+++ OneLastJob/OneLastJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneLastJobApp.swift
+//  OneLastJob
+//
+//  Created by Joshua Johnson on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct OneLastJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
